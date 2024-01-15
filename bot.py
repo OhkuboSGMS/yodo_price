e9b8208450c58ff5d8d2ef34c6e1900c30ac09d3
OhkuboSGMS <sinikake.gokiburi@gmail.com>
2024-01-15 21:14:21 +0900

    list内容を充実

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from sqlmodel import SQLModel, Session, select
 
 from yodo_price import update
 from yodo_price.get import get_product
-from yodo_price.model import Product
+from yodo_price.model import Product, Url
 from yodo_price.update import add_url
 from dotenv import load_dotenv
 
@@ -47,8 +47,9 @@ async def add(ctx, url: str):
 async def _list(ctx):
     try:
         with Session(engine) as session:
-            products: list[Product] = session.exec(select(Product).order_by(Product.id)).all()
-            product_msgs = list(map(lambda p: f"{p.name}", products))
+            products: list[tuple[Product, Url]] \
+                = session.exec(select(Product, Url).where(Product.id == Url.id).order_by(Product.id)).all()
+            product_msgs = list(map(lambda p: f"{p[0].name}:{p[1].url}", products))
     except Exception as e:
         print(e)
         await ctx.send(f"list failed:{e}")
