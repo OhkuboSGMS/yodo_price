aafec8cbe748f85a082f8db9ea13c2c88db9f378
OhkuboSGMS <ginger.os777146th@gmail.com>
2024-05-14 00:51:16 +0900

    比較処理を修正

--- yodo_price/check.py
+++ yodo_price/check.py
@@ -1,9 +1,20 @@
 from sqlmodel import Session
+from typing_extensions import Optional
 
 from yodo_price.model import Product
 from yodo_price.query import get_last_price
 
 
-def is_price_lower(session: Session, product: Product, price: int) -> bool:
+def is_price_lower(session: Session, product: Product, price: Optional[int]) -> bool:
+    """
+
+    :param session:
+    :param product:
+    :param price:更新前の価格
+    :return:
+    """
     latest_price = get_last_price(session, product)
-    return latest_price > price
+    print(f"最新価格:{latest_price},更新前の価格:{price},商品名:{product.name}")
+    if price is None:
+        return False
+    return latest_price < price
--- yodo_price/query.py
+++ yodo_price/query.py
@@ -1,4 +1,5 @@
 from sqlmodel import Session, select
+from typing_extensions import List, Optional, Tuple
 
 from yodo_price.model import Product, Price
 
@@ -13,3 +14,19 @@ def get_last_price(session: Session, product: Product) -> int:
         return last_price.price
     else:
         return 0
+
+
+def get_products_latest_price(session: Session) -> List[Tuple[Product, Optional[Price]]]:
+    """
+    DB内の最新の値段を取得する,UrlとProductを結合し、Priceをgroup byして最新の値段を取得する
+    """
+    products = session.exec(select(Product)).all()
+    results = []
+    for product in products:
+        statement = select(Price).where(Price.product_id == product.id).order_by(Price.date.desc()).limit(1)
+        last_price = session.exec(statement).one_or_none()
+        if last_price:
+            results.append((product, last_price))
+        else:
+            results.append((product, None))
+    return results
