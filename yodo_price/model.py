58ef2b00cf75934392eee5a6e3ca5891da4ae42d
OhkuboSGMS <ginger.os777146th@gmail.com>
2024-05-19 21:45:08 +0900

    refactoring

--- yodo_price/model.py
+++ yodo_price/model.py
@@ -6,6 +6,17 @@ from sqlmodel import SQLModel, Field, Relationship
 from yodo_price.constants import format_price, BASE_URL
 
 
+def get_short_product_name(product_name: Optional[int]):
+    category_info = str(product_name).find("[")
+    if category_info:
+        return product_name[:category_info]
+    return product_name
+
+
+def get_product_url(product_id: Optional[int]) -> str:
+    return f"{BASE_URL}/{product_id}"
+
+
 class Url(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     url: str
@@ -19,18 +30,9 @@ class Price(SQLModel, table=True):
     product_id: Optional[int] = Field(default=None, foreign_key="product.id")
     product: Optional["Product"] = Relationship(back_populates="price_history")
 
-    def get_short_product_name(self):
-        category_info = self.product.name.find("[")
-        if category_info:
-            return self.product.name[:category_info]
-        return self.product.name
-
-    def url(self):
-        return f"{BASE_URL}/{self.product.product_id}"
-
     def format(self) -> str:
-        product_name = self.get_short_product_name()
-        return f"{self.id} | 取得日: {self.date.strftime('%Y-%m-%d %H:%M:%S')}| 価格: {format_price(self.price)} | 商品名: {product_name} |[link]){self.url()} "
+        return (f"{self.id} | 取得日: {self.date.strftime('%Y-%m-%d %H:%M:%S')}| 価格: {format_price(self.price)} |"
+                f" 商品名: {get_short_product_name(self.product.name)} |[link]{get_product_url(self.product_id)} ")
 
 
 class Product(SQLModel, table=True):
@@ -49,4 +51,5 @@ class LatestPrice(SQLModel, table=False):
     date: datetime
 
     def format(self) -> str:
-        return f"{self.id} | 取得日: {self.date.strftime('%Y-%m-%d %H:%M:%S')}| 価格: {self.price:,}円 | 商品名: {self.name}"
+        return (f"{self.id} | 取得日: {self.date.strftime('%Y-%m-%d %H:%M:%S')}| 価格: {format_price(self.price)} |"
+                f" 商品名: {get_short_product_name(self.name)} |[link]{get_product_url(self.product_id)} ")
