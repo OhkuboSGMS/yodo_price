5634aecb47a41e4cc144441822610694bebaa578
OhkuboSGMS <ginger.os777146th@gmail.com>
2024-05-27 21:31:46 +0900

    価格通知方法を修正
    * 価格が変化したら通知する
    * すぐに商品ページに飛べるようリンクを追加

--- yodo_price/check.py
+++ yodo_price/check.py
@@ -7,7 +7,7 @@ from yodo_price.query import get_last_price
 
 def is_price_lower(session: Session, product: Product, price: Optional[int]) -> bool:
     """
-
+    価格が下がったかどうかを検知する
     :param session:
     :param product:
     :param price:更新前の価格
@@ -18,3 +18,18 @@ def is_price_lower(session: Session, product: Product, price: Optional[int]) -> 
     if price is None:
         return False
     return latest_price < price
+
+
+def is_price_change(session: Session, product: Product, price: Optional[int]) -> bool:
+    """
+    価格の変更を検知する
+    :param session:
+    :param product:
+    :param price:
+    :return:
+    """
+    latest_price = get_last_price(session, product)
+    print(f"最新価格:{latest_price},更新前の価格:{price},商品名:{product.name}")
+    if price is None:
+        return False
+    return latest_price != price
