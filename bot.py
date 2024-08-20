9535a68752b11fd9504699f28353a0b8e2b65ed2
OhkuboSGMS <ginger.os777146th@gmail.com>
2024-08-20 13:38:20 +0900

    fix

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ async def add(ctx: Interaction, url: str):
             url_model = add_url(url, session)
             url_list = [url_model.url]
             results, errors = update.update(url_list, session)
-            (_, product, price) = results[0]
+            _, product, price = results[0]
             if len(errors) > 0:
                 await ctx.response.send_message(f"add failed:{errors}")
                 return
