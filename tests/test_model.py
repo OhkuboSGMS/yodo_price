649b5b21527c4257f15aef3b568968ea136b06e9
OhkuboSGMS <ginger.os777146th@gmail.com>
2024-10-09 21:51:20 +0900

    test Product.enable

---
+++ tests/test_model.py
@@ -0,0 +1,20 @@
+from yodo_price.model import Url, Product
+from sqlmodel import select
+
+
+def test_add_product(session):
+    url = Url(url="https://www.yodobashi.com/product/100000001001357243/")
+
+    session.add(url)
+    session.commit()
+    session.refresh(url)
+    product = Product(id=url.id,
+                      name="パナソニック Panasonicアルカリ乾電池 EVOLTA（エボルタ） 単3形 8＋2本パック LR6EJSP/10S",
+                      product_id="100000001001357243", image="", enable=False)
+    session.add(product)
+    session.commit()
+    session.refresh(product)
+    query_product = session.exec(select(Product).where(Product.product_id == "100000001001357243")).one()
+
+    assert query_product.name == product.name
+    assert query_product.enable == False
